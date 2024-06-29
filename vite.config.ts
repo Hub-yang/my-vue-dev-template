789e6618d78c4030c482d1e46d2e85d78215943b
HuberyYang <78254474+Hub-yang@users.noreply.github.com>
2024-06-29 15:27:30 +0800

    feat: feat: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
@@ -6,12 +7,20 @@ import Components from 'unplugin-vue-components/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import UnoCSS from 'unocss/vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
   plugins: [
     VueRouter(),
     Vue(),
     VueDevTools(),
+    svgLoader(),
     AutoImport({
       imports: [
         'vue',
